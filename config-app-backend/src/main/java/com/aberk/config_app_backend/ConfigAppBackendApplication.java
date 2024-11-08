5f4515279816067aa524b1ebe2f03e80a444f059
berkalparslan <aberkalparslan@gmail.com>
2024-11-08 16:03:12 +0300

    init config app

---
+++ config-app-backend/src/main/java/com/aberk/config_app_backend/ConfigAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aberk.config_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigAppBackendApplication.class, args);
+	}
+
+}
